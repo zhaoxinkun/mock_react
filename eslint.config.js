08377e3e975ae6f8e69364418822c5ec3928f5d6
zhaoxinkun <zhaoxinkun1124@gmail.com>
2026-01-08 14:16:26 +0800

    vite.config

--- eslint.config.js
+++ eslint.config.js
@@ -8,16 +8,25 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    // 配置文件的作用范围
     files: ['**/*.{ts,tsx}'],
+
+    // 继承的配置
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite
     ],
+    // 语言选项
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser
+    },
+    rules: {
+      'no-unused-vars': 'error', // 禁止定义未使用的变量
+      'no-debugger': 'error', // 禁止使用debugger
+      'no-var': 'error' // 要求使用 let 或 const 而不是 var
     }
   }
 ])
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  publicDir: 'src',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  server: {
+    host: '127.0.0.1',
+    port: 8080,
+    open: true
+  },
+  plugins: [react()]
 })
